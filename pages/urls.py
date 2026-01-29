fa0e33a08a57b2f71e514a592fc8f6988086e07a
andre-2710 <andre.fragata@hotmail.com>
2026-01-28 22:31:38 -0300

    Atualiza projeto com Layout Base e Navegação com Django Templates - Trabalho 2

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name='home'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contato/', views.contato, name='contato'),
+    path('ajuda/', views.ajuda, name='ajuda'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-    contexto = {
-        "mensagem": "Servidor ligado e responsivo."
-    }
-    return render(request, "pages/home.html", contexto)
+    return render(request, 'pages/home.html', {
+        'mensagem': 'Bem-Vindo ao site.'
+    })
+
+def sobre(request):
+    return render(request, 'pages/sobre.html', {})
+
+def contato(request):
+    return render(request, 'pages/contato.html', {})
+
+def ajuda(request):
+    return render(request, 'pages/ajuda.html', {})
